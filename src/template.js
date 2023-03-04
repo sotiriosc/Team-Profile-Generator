dd719d27509d514f97d2aca27bc23e1e7aa01658
sotiriosc <chortogiannoss@gmail.com>
2023-03-04 18:03:38 -0500

    fixed texts corrected code and file names

--- src/template.js
+++ src/template.js
@@ -7,7 +7,7 @@ const generateTeam = team => {
 <div class="card employee-card">
     <div class="card-header bg-primary text-white">
         <h2 class="card-title">${manager.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        <h3 class="card-title">${manager.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -26,7 +26,7 @@ const generateTeam = team => {
 <div class="card employee-card">
     <div class="card-header bg-primary text-white">
         <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        <h3 class="card-title">${engineer.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -45,7 +45,7 @@ const generateTeam = team => {
 <div class="card employee-card">
     <div class="card-header bg-primary text-white">
         <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        <h3 class="card-title">${intern.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
---
+++ test/Manager.test.js
@@ -0,0 +1,20 @@
+// Test to see if manager is created correctly
+const Manager = require("../lib/Manager");
+
+test("Can create an office number.", () => {
+    const testOfficeNumber = 2;
+    const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", testOfficeNumber);
+    expect(employeeInstance.officeNumber).toBe(testOfficeNumber);
+});
+
+test("Testing officeNumber will return office number.", () => {
+    const testOfficeNumber = 2;
+    const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", testOfficeNumber);
+    expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Manager";
+    const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", 2);
+    expect(employeeInstance.getRole()).toBe(returnValue);
+});
